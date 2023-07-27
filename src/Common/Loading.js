cc8236cf559551610accac8a53f72f6df9c2a3ad
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-27 12:00:04 -0700

    Updated loader to utilized react-loaders-kit

--- src/Common/Loading.js
+++ src/Common/Loading.js
@@ -1,12 +1,22 @@
+import { GooeyCircleLoader } from "react-loaders-kit";
 
 /** Render loading message
  *
  * App/ -> Loading
  */
- function Loading() {
+function Loading() {
+  const loaderProps = {
+    loading: true,
+    size: 275,
+    duration: 2,
+    colors: ["#99fffe", "#f42e00", "#042549"],
+  };
+
   return (
-    <h1 className="mt-5">Loading...</h1>
-  )
+    <div className="loader">
+      <GooeyCircleLoader {...loaderProps} />
+    </div>
+  );
 }
 
 export default Loading;
