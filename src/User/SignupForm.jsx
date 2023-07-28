24feb12afb277b31104aa1c3e032ac1aa9593cd5
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-28 12:40:54 -0700

    add placeholders to signup form so guests have a smoother input process

--- src/User/SignupForm.jsx
+++ src/User/SignupForm.jsx
@@ -31,10 +31,10 @@ function Signup({ handleRegister }) {
   const initialState = {
     username: "",
     password: "",
-    interests: "",
-    hobbies: "",
-    location: "",
-    radius: 5,
+    interests: "Making all the friends!",
+    hobbies: "Testing people's applications, I swear I'm not a stalker",
+    location: "12345",
+    radius: 500,
   };
   const [inputData, setInputData] = useState(initialState);
   const [selectedFile, setSelectedFile] = useState(null);
@@ -75,121 +75,147 @@ function Signup({ handleRegister }) {
     }
   }
 
-  // if (currUser) return <Navigate to="/" />;
-
   return (
-    <div className="d-flex justify-content-center p-3">
+    <>
       {loading ? (
         <Loading />
       ) : (
-        <div className="col-lg-6 col-10">
-          <p className="text-center fs-3">Sign-up in just a few simple steps</p>
-          <form
-            onSubmit={handleSubmit}
-            className="bg-light rounded border border-dark border-2 shadow p-3"
-          >
-            <div className="form-group">
-              {err && <Alerts err={err} />}
-              <label className="d-flex float-left mx-1 pt-3" htmlFor="username">
-                <b>Username</b>
-              </label>
-              <input
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                id="username"
-                name="username"
-                value={inputData.username}
-                required
-              />
-              <label className="d-flex float-left mx-1 pt-3" htmlFor="password">
-                <b>Password</b>
-              </label>
-              <input
-                onChange={handleChange}
-                type="password"
-                className="form-control"
-                id="password"
-                name="password"
-                value={inputData.password}
-                required
-              />
-              <label className="d-flex float-left mx-1 pt-3" htmlFor="interests">
-                <b>Interests</b>
-              </label>
-              <textarea
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                id="interests"
-                name="interests"
-                value={inputData.interests}
-              />
-              <label className="d-flex flex-column float-left mx-1 pt-3" htmlFor="hobbies">
-                <b>Hobbies</b>
-              </label>
-              <textarea
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                id="hobbies"
-                name="hobbies"
-                value={inputData.hobbies}
-              />
-              <label className="d-flex flex-column float-left mx-1 pt-3" htmlFor="image">
-                <b>Image</b>
-                <p className="text-muted">JPG, PNG, & Webpb</p>
-              </label>
-              <input
-                onChange={handleFileSelect}
-                type="file"
-                className="form-control"
-                id="image"
-                name="image"
-                required
-              />
-              <label className="d-flex flex-column float-left mx-1 pt-3" htmlFor="location">
-                <b>Zip Code</b>
-                <p className="text-muted">We recommend 12345 for text users.</p>
-              </label>
-              <input
-                onChange={handleChange}
-                type="number"
-                className="form-control"
-                id="location"
-                name="location"
-                value={inputData.location}
-                required
-              />
-              <label className="d-flex flex-column float-left mx-1 pt-3" htmlFor="radius">
-                <b>Preferred Radius in Miles</b>
-                <p className="text-muted">We recommend 500 to start.</p>
-              </label>
-              <select
-                onChange={handleChange}
-                type="range"
-                className="form-control"
-                id="radius"
-                name="radius"
-                value={inputData.radius}
-              >
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-                <option value="250">250</option>
-                <option value="500">500</option>
-              </select>
-            </div>
-            <div className="d-flex justify-content-center">
-            <button type="submit" className="btn btn-dark btn-lg mt-3 ">
-              Join
-            </button>
-            </div>
-          </form>
+        <div className="d-flex justify-content-center p-3">
+          <div className="col-lg-6 col-10">
+            <p className="text-center fs-3">
+              Sign-up in just a few simple steps
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="bg-light rounded border border-dark border-2 shadow p-3"
+            >
+              <div className="form-group">
+                {err && <Alerts err={err} />}
+                <label
+                  className="d-flex float-left mx-1 pt-3"
+                  htmlFor="username"
+                >
+                  <b>Username</b>
+                </label>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  name="username"
+                  value={inputData.username}
+                  required
+                />
+                <label
+                  className="d-flex float-left mx-1 pt-3"
+                  htmlFor="password"
+                >
+                  <b>Password</b>
+                </label>
+                <input
+                  onChange={handleChange}
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={inputData.password}
+                  required
+                />
+                <label
+                  className="d-flex float-left mx-1 pt-3"
+                  htmlFor="interests"
+                >
+                  <b>Interests</b>
+                </label>
+                <textarea
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                  id="interests"
+                  name="interests"
+                  value={inputData.interests}
+                />
+                <label
+                  className="d-flex flex-column float-left mx-1 pt-3"
+                  htmlFor="hobbies"
+                >
+                  <b>Hobbies</b>
+                </label>
+                <textarea
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                  id="hobbies"
+                  name="hobbies"
+                  value={inputData.hobbies}
+                />
+                <label
+                  className="d-flex flex-column float-left mx-1 pt-3"
+                  htmlFor="image"
+                >
+                  <b>Image</b>
+                  <p className="text-muted">JPG, PNG, & Webpb</p>
+                </label>
+                <input
+                  onChange={handleFileSelect}
+                  type="file"
+                  className="form-control"
+                  id="image"
+                  name="image"
+                  required
+                />
+                <label
+                  className="d-flex flex-column float-left mx-1 pt-3"
+                  htmlFor="location"
+                >
+                  <b>Zip Code</b>
+                  <p className="text-muted">
+                    We recommend 12345 for text users.
+                  </p>
+                </label>
+                <input
+                  onChange={handleChange}
+                  type="number"
+                  className="form-control"
+                  id="location"
+                  name="location"
+                  value={inputData.location}
+                  required
+                />
+                <label
+                  className="d-flex flex-column float-left mx-1 pt-3"
+                  htmlFor="radius"
+                >
+                  <b>Preferred Radius in Miles</b>
+                  <p className="text-muted">We recommend 500 to start.</p>
+                </label>
+                <select
+                  onChange={handleChange}
+                  type="range"
+                  className="form-control"
+                  id="radius"
+                  name="radius"
+                  value={inputData.radius}
+
+                >
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                  <option value="250">250</option>
+                  <option value="500">500</option>
+                </select>
+              </div>
+              <div className="d-flex justify-content-center">
+                <button type="submit" className="btn btn-dark btn-lg mt-3 ">
+                  Join
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
