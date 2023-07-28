d9d55d1a5d85c422eda9bca7a3c5b95cc03226de
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-28 16:14:02 -0700

    Fix another deployment error with surge

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'mjs'],
     },
-    plugins: [react()],
-  };
+  },
 });
