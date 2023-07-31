dc15d7c526b78ddc62b1552383a08df938562709
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-31 12:36:22 -0700

    -wip deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import jwt_decode from "jwt-decode";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 
-import FrienderApi from "./helpers/api";
-import NavBar from './routes/NavBar'
-import RoutesList from "./routes/RoutesList";
-import Loading from "./common/Loading";
-import TOAST_DEFAULTS from "./helpers/toastSettings";
-import userContext from "./user/userContext";
+import FrienderApi from "@helpers/api";
+import NavBar from '@routes/NavBar'
+import RoutesList from "@routes/RoutesList";
+import Loading from "@common/Loading";
+import TOAST_DEFAULTS from "@helpers/toastSettings";
+import userContext from "@user/userContext";
 
 const LOCAL_STORAGE_TOKEN_KEY = "token";
 
