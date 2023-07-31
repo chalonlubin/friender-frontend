b27189a23e16d3610ee50babf0eb2bd576660947
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-31 12:35:19 -0700

    -wip deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import path from 'path'
-// import { fileURLToPath } from 'url';
+import path from 'path'
+import { fileURLToPath } from 'url';
 
-// const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 
-// const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     // Add an alias for the helpers directory
-  //     '@src': path.resolve(__dirname, 'src'),
-  //       '@root': path.resolve(__dirname, './'),
-  //       '@public': path.resolve(__dirname, './public'),
-  //       '@cypress': path.resolve(__dirname, './cypress'),
-  //       '@dist': path.resolve(__dirname, './dist'),
-  //     },
-  //   },
+
+  resolve: {
+    alias: {
+      // Add an alias for the helpers directory
+
+      '@src': path.resolve(__dirname, 'src'),
+        '@root': path.resolve(__dirname, './'),
+        '@public': path.resolve(__dirname, './public'),
+        '@cypress': path.resolve(__dirname, './cypress'),
+        '@dist': path.resolve(__dirname, './dist'),
+        "@helpers": path.resolve(__dirname, "src/helpers"),
+        "@routes": path.resolve(__dirname, "src/routes"),
+        "@common": path.resolve(__dirname, "src/common"),
+        "@user": path.resolve(__dirname, "src/user"),
+      },
+    },
   server: {
     port: 8080,
   },
