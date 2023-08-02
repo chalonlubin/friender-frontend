037642ef6c961a354fb1d04cfaf4cd20be03f5af
Chalon Lubin <chalonlubin1@gmail.com>
2023-08-02 11:42:53 -0700

    -wip deployment issues

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-
-console.log("META IMPORT", import.meta.env.VITE_API_BASE_URL)
-console.log("META INV", import.meta.env)
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
 /** Friender API Class. */
